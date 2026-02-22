f3383845173d806d624871e98890a26e99c8d332
Anjela Arora <anjela.arora@yourdategenie.com>
2026-02-21 21:52:58 -0500

    Initial commit - Your Date Genie

---
+++ Your Date Genie/Your Date GenieUITests/Your_Date_GenieUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Your_Date_GenieUITestsLaunchTests.swift
+//  Your Date GenieUITests
+//
+//  Created by Anjela Arora on 2/19/26.
+//
+
+import XCTest
+
+final class Your_Date_GenieUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
